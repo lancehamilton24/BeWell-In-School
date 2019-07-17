d49e892472362a443986c4b3c5e131e1788f6849
Lance Hamilton <lance.hamilton24@gmail.com>
2019-07-17 13:33:28 -0500

    added teachers to database

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using BeWell.Data;
+using BeWell.Models;
+//using BeWell.Validator;
 using Microsoft.AspNetCore.Mvc;
+using BeWell.Models.Teacher;
 
 namespace BeWell.Controllers
 {
@@ -11,5 +15,24 @@ namespace BeWell.Controllers
     [ApiController]
     public class TeacherController : ControllerBase
     {
+        readonly TeacherRepository _teacherRepository;
+        //readonly CreateTeacherRequestValidator _validator;
+        // readonly CreateCustomerProductValidator _customerProductValidator;
+
+        // GET: /<controller>/
+        public TeacherController()
+        {
+            //_validator = new CreateTeacherRequestValidator();
+            _teacherRepository = new TeacherRepository();
+        }
+        [HttpPost("register")]
+        public ActionResult AddTeacher(CreateTeacherRequest createRequest)
+        {
+               
+            var newTeacher = _teacherRepository.AddTeacher(createRequest.FirstName, createRequest.LastName, createRequest.Grade);
+            return Created($"/api/customers/{newTeacher.Id}", newTeacher);
+
+        }
+
     }
 }
--- Data/TeacherRepository.cs
+++ Data/TeacherRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BeWell.Models.Teacher;
+using System;
+using Dapper;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,26 @@ namespace BeWell.Data
 {
     public class TeacherRepository
     {
+        const string ConnectionString = "Server=localhost;Database=BeWell;Trusted_Connection=True;";
+
+        public Teacher AddTeacher(string firstName, string lastName, int grade)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var addTeacherInformation = db.QueryFirstOrDefault<Teacher>(@"
+                    Insert into teacher (firstName,lastName, grade)
+                    Output inserted.*
+                    Values(@firstName,@lastName,@grade)",
+                    new { firstName, lastName, grade });
+
+
+                if (addTeacherInformation != null)
+                {
+                    return addTeacherInformation;
+                }
+            }
+
+            throw new Exception("No user created");
+        }
     }
 }
