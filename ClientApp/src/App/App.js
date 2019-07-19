0fb949414507aee67f6b6913e5ee5e839bb27b0d
Lance Hamilton <lance.hamilton24@gmail.com>
2019-07-19 14:31:48 -0500

    added student and teacher portal components

--- ClientApp/src/App/App.js
+++ ClientApp/src/App/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { Layout } from '../components/Layout';
 import { Home } from '../components/Home/Home';
+import { TeacherPortal } from '../components/TeacherPortal/TeacherPortal';
+import { StudentPortal } from '../components/StudentPortal/StudentPortal';
 
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+      <BrowserRouter>
       <Layout>
+        <Switch>
         <Route exact path='/' component={Home} />
-
+      <Route path='/teacherPortal' component={TeacherPortal} />
+      <Route path='/studentPortal' component={StudentPortal} />
+      </Switch>
       </Layout>
       </BrowserRouter>
       </div>
---
+++ ClientApp/src/components/TeacherPortal/TeacherPortal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TeacherPortal extends Component {
+  render() {
+    return (
+      <div>
+       <h1>Teacher Portal</h1>;
+      </div>
+    );
+  }
+}
+
+export default TeacherPortal;
