5a06b3868d1fca563a533cf40f307cdc6123a729
Lance Hamilton <lance.hamilton24@gmail.com>
2019-07-26 12:51:48 -0500

    adding question to database

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BeWell.Data;
+using BeWell.Models.Question;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,20 @@ namespace BeWell.Controllers
     [ApiController]
     public class QuestionController : ControllerBase
     {
+        readonly QuestionRepository _questionRepository;
+
+
+        public QuestionController()
+        {
+            _questionRepository = new QuestionRepository();
+        }
+        [HttpPost("register")]
+        public ActionResult AddQuestion(CreateQuestionRequest createRequest)
+        {
+
+            var newQuestion = _questionRepository.AddQuestion(createRequest.QuestionText);
+            return Created($"/api/student/{newQuestion.Id}", newQuestion);
+
+        }
     }
 }
--- Data/QuestionRepository.cs
+++ Data/QuestionRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BeWell.Models.Question;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,26 @@ namespace BeWell.Data
 {
     public class QuestionRepository
     {
+        const string ConnectionString = "Server=localhost;Database=BeWell;Trusted_Connection=True;";
+
+        public Question AddQuestion(string questionText)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var addQuestion = db.QueryFirstOrDefault<Question>(@"
+                    Insert into Question (questionText)
+                    Output inserted.*
+                    Values(@questionText)",
+                    new { questionText });
+
+
+                if (addQuestion != null)
+                {
+                    return addQuestion;
+                }
+            }
+
+            throw new Exception("No question created");
+        }
     }
 }
