96e4278646518a10e189485e6579e77ad4b979b3
Lance Hamilton <lance.hamilton24@gmail.com>
2019-07-27 00:15:22 -0500

    deleting, adding, and reading answers in postman

--- Controllers/AnswerController.cs
+++ Controllers/AnswerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BeWell.Data;
+using BeWell.Models.Answer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,42 @@ namespace BeWell.Controllers
     [ApiController]
     public class AnswerController : ControllerBase
     {
+        readonly AnswerRepository _answerRepository;
+
+
+        public AnswerController()
+        {
+            _answerRepository = new AnswerRepository();
+        }
+        [HttpPost("register")]
+        public ActionResult AddTextAnswer(CreateAnswerRequest createRequest)
+        {
+
+            var newAnswer = _answerRepository.AddTextAnswer(createRequest.AnswerText);
+            return Created($"/api/student/{newAnswer.Id}", newAnswer);
+
+        }
+        [HttpPost("registerNumber")]
+        public ActionResult AddNumberAnswer(CreateAnswerRequest createRequest)
+        {
+
+            var newAnswer = _answerRepository.AddNumberAnswer(createRequest.AnswerNumber);
+            return Created($"/api/student/{newAnswer.Id}", newAnswer);
+
+        }
+        [HttpGet("allAnswers")]
+        public ActionResult GetAllAnswers()
+        {
+            var Answers = _answerRepository.GetAllAnswers();
+
+            return Ok(Answers);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteSingleAnswer(int id)
+        {
+            var deletedAnswer = _answerRepository.DeleteSingleAnswer(id);
+            return Ok(deletedAnswer);
+        }
     }
 }
--- Data/AnswerRepository.cs
+++ Data/AnswerRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BeWell.Models.Answer;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,67 @@ namespace BeWell.Data
 {
     public class AnswerRepository
     {
+        const string ConnectionString = "Server=localhost;Database=BeWell;Trusted_Connection=True;";
+
+        public Answer AddTextAnswer(string answerText)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var addAnswer = db.QueryFirstOrDefault<Answer>(@"
+                    Insert into Answer (answerText)
+                    Output inserted.*
+                    Values(@answerText)",
+                    new { answerText});
+
+
+                if (addAnswer != null)
+                {
+                    return addAnswer;
+                }
+            }
+
+            throw new Exception("No answer created");
+        }
+
+        public Answer AddNumberAnswer(int answerNumber)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var addAnswer = db.QueryFirstOrDefault<Answer>(@"
+                    Insert into Answer (answerNumber)
+                    Output inserted.*
+                    Values(@answerNumber)",
+                    new { answerNumber });
+
+
+                if (addAnswer != null)
+                {
+                    return addAnswer;
+                }
+            }
+
+            throw new Exception("No answer created");
+        }
+
+        public IEnumerable<Answer> GetAllAnswers()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var allAnswers = db.Query<Answer>(@"Select * from Answer").ToList();
+                return allAnswers;
+            }
+        }
+
+        public Answer DeleteSingleAnswer(int id)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var deletedAnswer = db.QueryFirstOrDefault<Answer>(@"delete
+                                                                       from Answer
+                                                                       where id = @id",
+                                                                       new { id });
+                return deletedAnswer;
+            }
+        }
     }
 }
