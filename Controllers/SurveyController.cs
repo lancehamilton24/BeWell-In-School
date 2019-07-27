c6e0bd540cce93ff3443a0b38f3e7fab923916c1
Lance Hamilton <lance.hamilton24@gmail.com>
2019-07-27 09:41:18 -0500

    adding survey from postman

--- Controllers/SurveyController.cs
+++ Controllers/SurveyController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BeWell.Data;
+using BeWell.Models.Survey;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,23 @@ namespace BeWell.Controllers
     [ApiController]
     public class SurveyController : ControllerBase
     {
+        readonly SurveyRepository _surveyRepository;
+        //readonly CreateTeacherRequestValidator _validator;
+        // readonly CreateCustomerProductValidator _customerProductValidator;
+
+        // GET: /<controller>/
+        public SurveyController()
+        {
+            //_validator = new CreateTeacherRequestValidator();
+            _surveyRepository = new SurveyRepository();
+        }
+        [HttpPost("register")]
+        public ActionResult AddSurvey(CreateSurveyRequest createRequest)
+        {
+
+            var newSurvey = _surveyRepository.AddSurvey(createRequest.Date, createRequest.QuestionId, createRequest.AnswerId);
+            return Created($"/api/student/{newSurvey.Id}", newSurvey);
+
+        }
     }
 }
