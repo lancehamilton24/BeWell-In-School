b19edc1fc8b98470460526d00004511a8f033bc0
Lance Hamilton <lance.hamilton24@gmail.com>
2019-07-27 10:05:17 -0500

    adding student surveys in postman

--- Controllers/StudentSurveyController.cs
+++ Controllers/StudentSurveyController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BeWell.Data;
+using BeWell.Models.StudentSurvey;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,24 @@ namespace BeWell.Controllers
     [ApiController]
     public class StudentSurveyController : ControllerBase
     {
+        readonly StudentSurveyRepository _studentSurveyRepository;
+        //readonly CreateTeacherRequestValidator _validator;
+        // readonly CreateCustomerProductValidator _customerProductValidator;
+
+        // GET: /<controller>/
+        public StudentSurveyController()
+        {
+            //_validator = new CreateTeacherRequestValidator();
+            _studentSurveyRepository = new StudentSurveyRepository();
+        }
+        [HttpPost("register")]
+        public ActionResult AddStudentSurvey(CreateStudentSurveyRequest createRequest)
+        {
+            
+            var newStudentSurvey = _studentSurveyRepository.AddStudentSurvey(createRequest.StudentId, createRequest.SurveyId, createRequest.TeacherComments);
+            return Created($"/api/student/{newStudentSurvey.Id}", newStudentSurvey);
+
+        }
+
     }
 }
--- Data/StudentSurveyRepository.cs
+++ Data/StudentSurveyRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BeWell.Models.StudentSurvey;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,26 @@ namespace BeWell.Data
 {
     public class StudentSurveyRepository
     {
+        const string ConnectionString = "Server=localhost;Database=BeWell;Trusted_Connection=True;";
+
+        public StudentSurvey AddStudentSurvey(int studentId, int surveyId, string teacherComments)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var addStudentSurvey = db.QueryFirstOrDefault<StudentSurvey>(@"
+                    Insert into StudentSurvey (studentId, surveyId, teacherComments)
+                    Output inserted.*
+                    Values(@studentId,@surveyId,@teacherComments)",
+                    new { studentId, surveyId, teacherComments });
+
+
+                if (addStudentSurvey != null)
+                {
+                    return addStudentSurvey;
+                }
+            }
+
+            throw new Exception("No user created");
+        }
     }
 }
