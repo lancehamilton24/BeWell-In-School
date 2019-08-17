6ab2f0eae692d402877cc23e1aa312a9d0d77c39
Lance Hamilton <lance.hamilton24@gmail.com>
2019-08-17 11:37:57 -0500

    getting answers in state

--- ClientApp/src/components/SurveyResponses/SurveyResponses.js
+++ ClientApp/src/components/SurveyResponses/SurveyResponses.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import answerRequest from '../../helpers/data/answerRequest';
 
 export class SurveyResponses extends Component {
+  state ={
+    currentStudentId: this.props.location.state.selectedStudentId,
+    answers: []
+  }
+
+  getAnswersByStudentId = () => {
+    const { currentStudentId } = this.state
+    answerRequest.getAnswersByStudentRequest(currentStudentId).then((answers) => {
+      this.setState({ answers })
+    })
+  }
+
+  componentDidMount() {
+    this.getAnswersByStudentId();
+  }
+
   render() {
     return (
       <div>
